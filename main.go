802e013fb7d41b6e646f6dc2548e2fa53df5b667
regulknight <AunwovImnic4>
2021-05-16 22:29:22 +0400

    Adding first version of websocket multiplayer

--- main.go
+++ main.go
@@ -3,19 +3,21 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"image"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
-
-	"golang.org/x/image/colornames"
+	"os/signal"
+	"time"
 
 	_ "image/png"
 
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"github.com/gorilla/websocket"
-	_ "golang.org/x/image/colornames"
+	"golang.org/x/image/colornames"
 )
 
 func loadPicture(path string) (pixel.Picture, error) {
@@ -38,26 +40,28 @@ type Player struct {
 }
 
 type World struct {
-	players [4]*Player
+	Players [4]*Player
 }
 
 type LocalContext struct {
 	currentPlayer int
 }
 
+var playerId int
+
 func getCurrentPlayerId() int {
-	return 0
+	return playerId
 }
 
 func updatePlayerPosition(playerId int, playerPosition pixel.Matrix) {
-	world.players[playerId].DrawMatrix = playerPosition
+	world.Players[playerId].DrawMatrix = playerPosition
 }
 
 var world World
 
 func run() {
 	cfg := pixelgl.WindowConfig{
-		Title:  "Pixel Rocks!",
+		Title:  "Pixel Rocks! Player " + fmt.Sprint(getCurrentPlayerId()),
 		Bounds: pixel.R(0, 0, 1024, 768),
 		VSync:  true,
 	}
@@ -67,7 +71,7 @@ func run() {
 		panic(err)
 	}
 
-	pic, err := loadPicture("players.png")
+	pic, err := loadPicture("resources/players.png")
 	if err != nil {
 		panic(err)
 	}
@@ -79,12 +83,12 @@ func run() {
 	players[3] = &Player{pixel.NewSprite(pic, pixel.R(148, 0, 196, 50)), pixel.IM.Moved(pixel.Vec{X: 750, Y: 500}), 3}
 
 	localContext := &LocalContext{getCurrentPlayerId()}
-	world.players = players
+	world.Players = players
 
 	if getCurrentPlayerId() == 0 {
-		hostServer()
+		go hostServer()
 	} else {
-		runClient()
+		go runClient()
 	}
 
 	for !win.Closed() {
@@ -94,7 +98,7 @@ func run() {
 
 		updatePlayerPosition(localContext.currentPlayer, pixel.IM.Moved(win.MousePosition()))
 
-		for player := range world.players {
+		for player := range world.Players {
 			players[player].sprite.Draw(win, players[player].DrawMatrix)
 		}
 	}
@@ -109,9 +113,52 @@ func hostServer() {
 	http.HandleFunc("/player", serverPlayerPosition)
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
-
 func runClient() {
+	flag.Parse()
+	log.SetFlags(0)
 
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/player"}
+	log.Printf("connecting to %s", u.String())
+
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	ticker := time.NewTicker(10)
+
+	for t := range ticker.C {
+		msg, err := json.Marshal(world.Players[getCurrentPlayerId()])
+		if err != nil {
+			log.Println("write:", err)
+			log.Println(t)
+			break
+		}
+
+		err = c.WriteMessage(2, msg)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			log.Println(mt)
+			break
+		}
+
+		serverWorld := &World{}
+		json.Unmarshal(message, serverWorld)
+
+		for i, player := range serverWorld.Players {
+			updatePlayerPosition(i, player.DrawMatrix)
+		}
+	}
 }
 
 func serverPlayerPosition(w http.ResponseWriter, r *http.Request) {
@@ -127,7 +174,6 @@ func serverPlayerPosition(w http.ResponseWriter, r *http.Request) {
 			log.Println("read:", err)
 			break
 		}
-		log.Printf("recv: %s", message)
 
 		player := &Player{}
 		json.Unmarshal(message, player)
@@ -135,6 +181,11 @@ func serverPlayerPosition(w http.ResponseWriter, r *http.Request) {
 		updatePlayerPosition(player.PlayerId, player.DrawMatrix)
 
 		msg, err := json.Marshal(world)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+
 		err = c.WriteMessage(mt, msg)
 		if err != nil {
 			log.Println("write:", err)
@@ -144,5 +195,7 @@ func serverPlayerPosition(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fmt.Fscan(os.Stdin, &playerId)
+
 	pixelgl.Run(run)
 }
